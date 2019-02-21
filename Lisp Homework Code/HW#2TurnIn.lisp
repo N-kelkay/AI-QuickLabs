75627e3a443282792fa5a03ab1bb1e6a1c537326
Nate Kelkay <natikelkay@gmail.com>
2019-02-20 22:57:39 -0500

    done with 3 and 4

--- Lisp Homework Code/HW#2TurnIn.lisp
+++ Lisp Homework Code/HW#2TurnIn.lisp
@@ -1,5 +1,30 @@
-(defun mystery-fun-4 (x))
+; Natenael Kelkay
+; Lisp homework #2
 
-(defun mystery-dun-5 (x)
-  (dolist x
-  
+;3
+(defun mystery-fun-4 (x)
+  (let ((result 1))
+    (dotimes (i x)
+      (setf result (* result (+ i 1)))) result))
+
+;4
+(defun mystery-fun-5 (x)
+  (let ((counter 0))
+    (dolist (i x counter)
+      (setf counter (+ counter 1))) counter))
+
+;5
+(defun mystery-fun-6 (x)
+  (let ((result (append '() '()))
+    (loop
+      (cons )))))
+
+;7
+(defun question-6 (input)
+  (let ((result (append '() '())))
+    (dotimes (i x result)
+      (integerp i (setf result (cons x result)))) result))
+
+(question-6 '(a 1 2 b 3))
+
+
