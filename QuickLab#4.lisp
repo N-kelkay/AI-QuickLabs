9f0ed4a2aa6e46359247b90efc792689fedc2f85
Nate Kelkay <natikelkay@gmail.com>
2019-03-14 08:43:04 -0400

    finished quicklab 4

--- QuickLab#4.lisp
+++ QuickLab#4.lisp
@@ -20,3 +20,34 @@
                
                (setf tempMax indArg)))))
 
+;2
+(defun calcVal (obj list)
+  (dolist (value list)
+    (print value)
+    (format t "~8d" (funcall obj value))))
+
+;3
+(defun sq (x)
+  (* x x))
+
+(defun recp (x)
+  (/ 1 x))
+
+(defun n-even (x)
+  (cond
+   ((not (oddp x)) x)
+   (t (+ x 1))))
+
+;4
+(defun tableP (obj low up)
+    (let ((numlist '()))
+    (setf numList (cons low numList))
+    (loop
+      (unless (not (equalp low up)) (return numList))
+      (setf low (+ low 1))
+      (setf numList (cons low numList)))
+      (setf numlist (cdr numlist))
+      (setf numlist (reverse numlist))
+         
+      (funcall obj 'sq numlist)
+      ))
